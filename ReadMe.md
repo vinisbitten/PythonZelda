06d63542125d92adef5c68fc3a125fa37fb47a12
Vinícius Bittencourt <bittencourt1310@gmail.com>
2022-08-16 01:25:07 -0300

    v0.1

--- ReadMe.md
+++ ReadMe.md
@@ -4,4 +4,11 @@
 
 status: ongoing
 
-v0.0 - only hello on screen bo uncommenting the debug tests ✅
+v0.0 ✅
+    * only hello on screen bo uncommenting the debug tests
+
+v0.1 ✅
+    * added window name ('PyZelda') 
+    * created basic map 
+    * created static lvl 1
+    * added all visual stuff
--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import pygame, sys
 from settings import *
-
-# debug test (pass)
-# from debug import debug
+from level import Level
 
 class Game:
     def __init__(self):
@@ -12,6 +10,11 @@ class Game:
         self.screen = pygame.display.set_mode((WIDTH, HEIGTH))
         self.clock = pygame.time.Clock()
 
+        #game name on window
+        pygame.display.set_caption('PyZelda')
+
+        self.level = Level()
+
     def run(self):
         while True:
             for event in pygame.event.get():
@@ -20,12 +23,10 @@ class Game:
                     sys.exit()
             
             self.screen.fill('Black')
-
-            # debug test (pass)
-            # debug('hello')
-
+            self.level.run()
             pygame.display.update()
             self.clock.tick(FPS)
+
 if __name__ == '__main__':
     game = Game()
     game.run()
